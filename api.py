0194b22ab70fb048e9029ac14c3ea7d33eba912b
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-07-06 23:28:24 -0700

    Terrain server updated with Redis routes

--- api.py
+++ api.py
@@ -1,46 +1,77 @@
 import eventlet
 eventlet.monkey_patch()
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 import traceback
 
 import eventlet.wsgi
+import numpy as np
 
-from create_terrain_objects import save_terrain, create_or_update_terrain_object, get_all_food, get_all_obstacles
+# from create_terrain_objects import save_terrain, create_or_update_terrain_object, get_all_food, get_all_obstacles
 
 app = Flask(__name__)
 
-app.config.from_object('config.development')
 # Absolute path to the configuraiton file
 app.config.from_envvar('APP_CONFIG_FILE')
+print(app.config['DB_URL'] + '/' + 'food' + '/add' + str(3))
 
+height = 250
+width = 250
 
 @app.route('/')
 def test_connect():
     return "connected to objects service"
 
-## Remove this once everyone is updated
 @app.route('/terrain_objects', methods=['GET'])
 def get_terrain_objects():
-    requests.post('http://localhost:9000/send_field_objects', json ={'food':get_all_food(), 'obstacles': get_all_obstacles()})
+    global height, width
+    height = int(request.args.get('height'))
+    width = int(request.args.get('width'))
+    requests.post(app.config['SOCKET_URL'] + '/send_field_objects', json ={'food':get_all_food(), 'obstacles': get_all_obstacles()})
     return 'OK'
 
 @app.route('/update_object', methods=['GET'])
 def update_object():
     objId = request.args.get('id')
     objType = request.args.get('type')
-    data = create_or_update_terrain_object(objType, objId)
-    requests.post(app.config['DB_URL'] + '/' + objType + '/add', json={'id': objId})
-    return data
+    obj = random_coordinates(objId)
+    print(objId, objType, obj)
+    requests.post(app.config['DB_URL'] + '/' + objType + '/add', json={str(objType):obj})
+    return jsonify(obj)
 
-@app.route('/store_terrain', methods=['POST'])
-def save_landscape():
-    height = 250
-    width = 250
-    save_terrain(height, width, request.json["terrain"])
-    return 'Ok'
+######## ----- Helper Functions ------- ########
 
+def get_all_food():
+    print('get all food')
+    food = requests.get(app.config['DB_URL'] + '/food/get_all').json()
+    print('food', food)
+    if len(food) == 0:
+        for i in range(100):
+            coordinates = random_coordinates(i)
+            food.append(coordinates)
+            requests.post(app.config['DB_URL'] + '/food/add', json={'food': [coordinates]})
+    return food
+
+def get_all_obstacles():
+    print('get all obstacles')
+    obstacles = requests.get(app.config['DB_URL'] + '/obstacles/get_all').json()
+    print('obstacles', obstacles)
+    if len(obstacles) == 0:
+        for j in range(15):
+            coordinates = random_coordinates(j)
+            obstacles.append(coordinates)
+            requests.post(app.config['DB_URL'] + '/obstacles/add', json={'obstacles': [coordinates]})
+    return obstacles
+
+def random_coordinates(num):
+  global height
+  print(height)
+  coordinates = np.random.randint(height, size=2).tolist()
+  id = num
+  x = coordinates[0]
+  z = coordinates[1]
+  return {'x': x, 'z': z, 'id': id}
     
 # error handling
 @app.errorhandler(500)
